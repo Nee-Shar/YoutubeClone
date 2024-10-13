686cc631103b47ff8a32850b84cb03c2454e2966
Neeraj Sharma <neeraj716sharma@gmail.com>
2024-10-13 22:49:13 +0530

    Sign In done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -66,7 +66,9 @@ const registerUser = asyncHandler(async (req, res) => {
     avatar: avatar.url,
     email,
     username: username.toLowerCase(),
-    coverImage: cover?.url || "https://static-cse.canva.com/blob/1145215/1.magebyRodionKutsaevviaUnsplash.jpg",
+    coverImage:
+      cover?.url ||
+      "https://static-cse.canva.com/blob/1145215/1.magebyRodionKutsaevviaUnsplash.jpg",
     password,
   });
 
@@ -83,4 +85,87 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (e) {
+    throw new ApiError(500, "Token generation failed from server side");
+  }
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+  //todos
+  // take data from req body
+  //validate email , username
+  // find user
+  // check password
+  // generate access and refresh token
+  // send cookie
+  const { email, username, password } = req.body;
+  if (!email || !username) {
+    //either usernam+pss or email+pss is reqd
+    throw new ApiError(400, "Email or username is required");
+  }
+
+  if (!password) {
+    throw new ApiError(400, "Password is required");
+  }
+
+  const user = await User.findOne({
+    $or: [
+      {
+        email,
+      },
+      { username },
+    ],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isValid = await user.isPasswordCorrect(password);
+
+  if (!isValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  // to dos 
+  // find remove refrsh token , reset cookies
+  
+});
+
+export { registerUser, loginUser, logoutUser };
